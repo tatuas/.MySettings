eb739be45154b3d0bab64bfd8fb011342921762a
Tatsuya Sawai <a209050@gmail.com>
2013-11-14 04:37:23 +0900

    Add install shell

---
+++ install.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+ln -s $HOME/.MySettings/.vimrc $HOME/.vimrc
+ln -s $HOME/.MySettings/.bashrc $HOME/.bashrc
+ln -s $HOME/.MySettings/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/.MySettings/.vrapperrc $HOME/.vrapperrc
+ln -s $HOME/.MySettings/.gitconfig $HOME/.gitconfig
